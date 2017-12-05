a1fb762ffd0d2e0e37826e7b462bb819b0513c1c
mikehalverson11 <mikehalverson916@gmail.com>
2017-12-05 14:33:52 -0800

    debug+CIs

--- BayesianRegression.R
+++ BayesianRegression.R
@@ -10,7 +10,7 @@ b0=.01
 sds=rinvgamma(numberOfSims,.01,.01)
 meanguess=rep(0,numberOfVariables)
 covariance0=matrix(c(1,0,0,0,1,0,0,0,1),numberOfVariables,numberOfVariables)
-betaorig=mvrnorm(numberOfSims,meanguess,covariance0)
+betaorig=mvrnorm(numberOfSims,meanguess,sds*covariance0)
 data=read_csv("C:/Users/Michael/Downloads/nbadata.csv")
 data$Intercept=1
 theta1=solve(solve(covariance0)+t(as.matrix(data[specifiedVariables]))%*%as.matrix(data[,specifiedVariables]))%*%
@@ -20,7 +20,7 @@ covariance1=solve((solve(covariance0)+t(as.matrix(data[specifiedVariables]))
                    %*%as.matrix(data[,specifiedVariables])))
 a1=a0+.5*nrow(data)
 b1=b0+.5*(t(betaorig)%*%solve(covariance0)%*%betaorig+as.matrix(t(data[,response]))%*%as.matrix(data[,response])-
-            t(theta1)%*%solve(covariance0)%*%theta1)
+            t(theta1)%*%solve(covariance1)%*%theta1)
 predictions=t(theta1)%*%t(as.matrix(data[,specifiedVariables]))
 MSE=(1/nrow(data))*sum((predictions-data[,response])^2)
 R2=1-sum((predictions-data[,response])^2)/(sum((data[,response]-mean(as.matrix(data[,response])))^2))
@@ -33,7 +33,13 @@ fit$coefficients;theta1
 sims1=10000
 
 sds1=rinvgamma(sims1,a1,b1)
-
-
-
-
+mean(sds1);sort(sds1)[.025*sims1];sort(sds1)[.975*sims1]
+betafinal=mvrnorm(sims1,theta1,sds*covariance1)
+for (i in 1:length(specifiedVariables)){
+  current=specifiedVariables[i]
+  message(current)
+  message(mean(betafinal[,current]))
+  message(sd(betafinal[,current]))
+  message(sort(betafinal[,current])[.025*sims1])
+  message(sort(betafinal[,current])[.975*sims1])
+}
